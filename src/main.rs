026444be224fd2a44b9712b3756c7a3285c9f8c9
broot <bananarobot@naver.com>
2019-12-14 17:07:58 +0900

    Add more texts ready

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![recursion_limit = "128"]
 
+mod texts;
+
 use rstyping::*;
 
 use yew::events::IKeyboardEvent;
@@ -28,27 +30,7 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        let content = "그래도 지구는 돈다
-나는 생각한다 고로 나는 존재한다
-나치가 그들을 덮쳤을 때
-달리는 기차 위에 중립은 없다
-당신들을 묻어버리겠다
-만국의 노동자여 단결하라
-모든 국가는 그에 걸맞은 정부를 가진다
-박수칠 때 떠나라
-빛이 있으라
-빵이 없으면 케이크를 먹으면 되지
-신은 죽었다
-싸움이 급하니 나의 죽음을 알리지 말라
-역사를 잊은 민족에게 미래는 없다
-왔노라, 보았노라, 이겼노라
-이것 또한 지나가리라
-일찍 일어나는 새가 벌레를 잡는다
-적의 적은 나의 친구
-종교는 인민의 아편이다
-죄는 미워하되 사람은 미워하지 말라
-주사위는 던져졌다"
-            .into();
+        let content = texts::texts();
 
         Model {
             value: "".into(),
