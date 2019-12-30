01cf72a91645237185d7954dfecf73d5936c44ad
broot <bananarobot@naver.com>
2019-12-30 09:54:35 +0900

    Fix deploy.sh to push to remote

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-git checkout master
+git checkout gh-pages
+git merge master
 cargo web deploy --release
 mv target/deploy/* ./
 git add index.html style.css rstyping.js rstyping.wasm
 git commit -m "updated on $(date)"
+git push origin gh-pages
 git checkout dev
