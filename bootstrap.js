10e2fa7ea5b2e5ad87b8c1ec3adf4d42a6032795
broot <bananarobot@naver.com>
2020-01-14 14:07:10 +0900

    Clean remove static/index.js and move code to bootstrap.js and some fixes

--- bootstrap.js
+++ bootstrap.js
@@ -1,1 +1,87 @@
-import('./pkg').then(module => {module.run_app();});
+import Chart from "chart.js";
+
+async function main() {
+  await import("./pkg").then(module => {
+    module.run_app();
+  });
+
+  let ctx = document.getElementById("chart");
+
+  let aChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      datasets: [
+        {
+          label: "Typing speed",
+          yAxisID: "A",
+          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        },
+        {
+          label: "Accuracy",
+          yAxisID: "B",
+          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        }
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            id: "A",
+            type: "linear",
+            position: "left",
+            ticks: {
+              max: 1000,
+              min: 0
+            }
+          },
+          {
+            id: "B",
+            type: "linear",
+            position: "right",
+            ticks: {
+              max: 100,
+              min: 0
+            }
+          }
+        ]
+      }
+    }
+  });
+
+  let accuracyData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let typingSpeedData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  setInterval(function() {
+    let tempArray = document.getElementById("result").innerText.split(" ");
+
+    let accuracy = parseInt(tempArray[0].slice(0));
+    let typingSpeed = parseInt(tempArray[1]);
+
+    if (isNaN(accuracy) || isNaN(typingSpeed)) {
+      accuracy = 0;
+      typingSpeed = 0;
+    }
+
+    accuracyData.shift();
+    typingSpeedData.shift();
+
+    accuracyData.push(accuracy);
+    typingSpeedData.push(typingSpeed);
+
+    console.log(aChart.data.datasets[0].data);
+    console.log(aChart.data.datasets[1].data);
+
+    redrawChart(aChart, accuracyData, typingSpeedData);
+  }, 1000);
+
+  function redrawChart(chart, accuracyData, typingSpeedData) {
+    chart.data.datasets[0].data = typingSpeedData;
+    chart.data.datasets[1].data = accuracyData;
+
+    chart.update();
+  }
+}
+
+main();
