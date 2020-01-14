c48f6f7ffaa3419b117dd5cea9bd1e0acfbb69e0
broot <bananarobot@naver.com>
2020-01-14 14:09:47 +0900

    Clean webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
-const path = require('path');
-const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const distPath = path.resolve(__dirname, 'dist');
+const distPath = path.resolve(__dirname, "dist");
+
 module.exports = (env, argv) => {
-    return {
-        devServer: {
-            contentBase: distPath,
-            compress: argv.mode === 'production',
-            port: 8000
-        },
-        entry: './bootstrap.js',
-        output: {
-            path: distPath,
-            filename: 'rstyping.js',
-            webassemblyModuleFilename: 'rstyping.wasm'
-        },
-        plugins: [
-            new CopyWebpackPlugin([{from: './static', to: distPath}]),
-            new WasmPackPlugin({
-                crateDirectory: '.',
-                extraArgs: '--no-typescript',
-            })
-        ],
-        watch: argv.mode !== 'production'
-    };
+  return {
+    devServer: {
+      contentBase: distPath,
+      compress: argv.mode === "production",
+      port: 8000
+    },
+    entry: "./bootstrap.js",
+    output: {
+      path: distPath,
+      filename: "rstyping.js",
+      webassemblyModuleFilename: "rstyping.wasm"
+    },
+    plugins: [
+      new CopyWebpackPlugin([{ from: "./static", to: distPath }])
+      //new WasmPackPlugin({ crateDirectory: "." })
+    ],
+    watch: argv.mode !== "production"
+  };
 };
